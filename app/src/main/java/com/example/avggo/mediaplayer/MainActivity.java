a6a4aeed660857b40ca0fbddef3798a530c679d5
Patrick Tobias <patrick_tobias@outlook.com>
2016-03-24 21:58:29 +0800

    Copies images from in-app assets to local storage at Pictures folder

--- app/src/main/java/com/example/avggo/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/avggo/mediaplayer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.avggo.mediaplayer;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,10 @@ import android.view.View;
 import android.widget.Button;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
     Button setupServerButton, connectServerBtn;
@@ -19,8 +24,58 @@ public class MainActivity extends AppCompatActivity {
 
         File newFolder = new File (Environment.getExternalStorageDirectory() + "/Pictures/MediaPlayer");
 
-        if (!newFolder.exists())
+        if (!newFolder.exists()) { // move sample pictures to local storage
             newFolder.mkdir(); // Create new folder under Pictures folder for local App usage
+        }
+
+        AssetManager assetManager = getAssets();
+
+        String[] files = null;
+
+        try {
+            files = assetManager.list("");
+        } catch (IOException e) {
+
+        }
+
+        if (files != null) for (String filename : files) {
+            InputStream IStream = null;
+            OutputStream OStream = null;
+
+            try {
+                IStream = assetManager.open(filename);
+                File OFile = new File(Environment.getExternalStorageDirectory() + "/Pictures/MediaPlayer/", filename);
+
+                OStream = new FileOutputStream(OFile);
+                byte[] buffer = new byte[1024];
+
+                int read;
+
+                while((read = IStream.read(buffer)) != -1){
+                    OStream.write(buffer, 0, read);
+                }
+
+            } catch (IOException ex) {
+
+            }
+
+            finally {
+                if (IStream != null) {
+                    try {
+                        IStream.close();
+                    } catch (IOException ex) {
+
+                    }
+                }
+                if (OStream != null) {
+                    try {
+                        OStream.close();
+                    } catch (IOException ex) {
+
+                    }
+                }
+            }
+        }
 
         setupServerButton = (Button)findViewById(R.id.setupServerButton);
         connectServerBtn = (Button)findViewById(R.id.connectButton);
