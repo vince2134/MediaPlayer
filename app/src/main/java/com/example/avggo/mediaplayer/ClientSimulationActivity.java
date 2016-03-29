a60fa6510423bb27ed4d4f174c5dc0a5205bccde
Kevin Chan <kevin98gray@gmail.com>
2016-03-29 23:55:33 +0800

    Create activity for client simulation settings

---
+++ app/src/main/java/com/example/avggo/mediaplayer/ClientSimulationActivity.java
@@ -0,0 +1,65 @@
+package com.example.avggo.mediaplayer;
+
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class ClientSimulationActivity extends AppCompatActivity {
+
+    EditText etLossProbability;
+    EditText etTimeout;
+    EditText etDelay;
+    RadioGroup rgVerbosity;
+    Button btnSave;
+    Button btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_simulation);
+        initializeHandlers();
+    }
+
+    protected void initializeHandlers() {
+        etLossProbability = (EditText) findViewById(R.id.etLossProbability);
+        etTimeout = (EditText) findViewById(R.id.etTimeout);
+        etDelay = (EditText) findViewById(R.id.etDelay);
+        rgVerbosity = (RadioGroup) findViewById(R.id.rgVerbosity);
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+
+        RadioButton button;
+
+        button = new RadioButton(this);
+        button.setText("Level 1");
+        rgVerbosity.addView(button);
+
+        button = new RadioButton(this);
+        button.setText("Level 2");
+        rgVerbosity.addView(button);
+
+        button = new RadioButton(this);
+        button.setText("Level 3");
+        rgVerbosity.addView(button);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
