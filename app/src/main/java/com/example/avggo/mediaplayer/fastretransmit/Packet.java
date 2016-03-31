2f93c68e619708059829fad01207cffc7e21d361
Patrick Tobias <patrick_tobias@outlook.com>
2016-03-31 20:49:41 +0800

    Add ack and packet classes sutied for Fast Retransmit

---
+++ app/src/main/java/com/example/avggo/mediaplayer/fastretransmit/Packet.java
@@ -0,0 +1,50 @@
+package com.example.avggo.mediaplayer.fastretransmit;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+/**
+ * Created by patricktobias on 3/31/16.
+ */
+public class Packet implements Serializable {
+
+    private int seqNo;
+    private byte[] data;
+    private boolean last;
+
+    public Packet (int seqNo, byte[] data, boolean last) {
+        super();
+        this.seqNo = seqNo;
+        this.data = data;
+        this.last = last;
+    }
+
+    public int getSeqNo () {
+        return seqNo;
+    }
+
+    public byte[] getData () {
+        return data;
+    }
+
+    public boolean isLast () {
+        return last;
+    }
+
+    public void setSeqNo (int seqNo) {
+        this.seqNo = seqNo;
+    }
+
+    public void setData (byte[] data) {
+        this.data = data;
+    }
+
+    public void setLast (boolean alst) {
+        this.last = last;
+    }
+
+    @Override
+    public String toString() {
+        return "Packet { seq = " + seqNo +", data = " + Arrays.toString(data) + ", last = " + last + "}";
+    }
+}
